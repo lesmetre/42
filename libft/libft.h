c629abf70a76f879d71bf9268c71501eb32540e0
mpressen <mpressen@student.42.fr>
2015-12-04 01:15:57 +0100

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -54,7 +54,6 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
