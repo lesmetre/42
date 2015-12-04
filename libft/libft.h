5df2021862907c1703d0d4cb96ac7a8d431d9155
mpressen <mpressen@student.42.fr>
2015-12-04 21:10:05 +0100

    Update libft.h

--- libft/libft.h
+++ libft/libft.h
@@ -39,7 +39,7 @@ int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strdup(const char *s1);
 int		ft_strequ(char const *s1, char const *s2);
-char	*ft_strlcat(char *dst, const char *src, size_t size);
+size_t  ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *dst, const char *src, size_t n);
