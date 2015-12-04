296e70e6f849554eb9ed39bb0d52c92a77513ff5
mpressen <mpressen@student.42.fr>
2015-12-04 20:46:04 +0100

    correction legere

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,27 +6,27 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 23:58:49 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/02 23:53:45 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/04 20:44:50 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t count1;
 	size_t count2;
 
 	count1 = 0;
 	count2 = 0;
-	while (s1[count1])
+	while (dst[count1])
 		count1++;
-	while (s2[count2] && count1 < size - 1)
+	while (ssrc[count2] && count1 < size - 1)
 	{
-		s1[count1] = s2[count2];
+		dst[count1] = src[count2];
 		count1++;
 		count2++;
 	}
-	s1[count1] = '\0';
+	dst[count1] = '\0';
 	return (s1);
 }
