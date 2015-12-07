8d8a750a957ed378a8977261b9a89a9a69025af4
mpressen <mpressen@student.42.fr>
2015-12-07 01:38:52 +0100

    correction pour ne pas syst null termine

--- libft/ft_strncpy.c
+++ libft/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/30 23:42:15 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/06 23:06:16 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/07 01:37:43 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,12 @@ char	*ft_strncpy(char *dst, const char *src, size_t n)
 	size_t count;
 
 	count = 0;
-	if (!dst && !src)
-		return (NULL);
 	while (src[count] && count < n)
 	{
 		dst[count] = src[count];
 		count++;
 	}
-	while (count <= n)
+	while (count < n)
 	{
 		dst[count] = '\0';
 		count++;
