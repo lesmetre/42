b9d873ba34b13e686fd335174255e802ebd2a4b7
mpressen <mpressen@student.42.fr>
2015-12-07 05:49:49 +0100

    ajout de la bonne condition

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 01:38:39 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/07 01:40:05 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/07 05:47:38 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,18 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*s11;
 	unsigned char	*s22;
 
+	if (!(s1) || !(s2) || !(n))
+		return (0);
 	s11 = (unsigned char *)s1;
 	s22 = (unsigned char *)s2;
 	s1len = ft_strlen(s1);
 	s2len = ft_strlen(s2);
-	if (s1len <= n)
-		n = s1len;
-	if (s2len <= n)
-		n = s2len;
+	if (s1len < n && s2len < n)
+	{
+		if (s1len > s2len)
+			n = s1len;
+		else
+			n = s2len;
+	}
 	return (memcmp(s11, s22, n));
 }
