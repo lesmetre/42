785a80901dce0d477f0165b47c069ef8562d08ec
mpressen <mpressen@student.42.fr>
2015-12-07 04:15:12 +0100

    lol

--- libft/ft_strsplit.c
+++ libft/ft_strsplit.c
@@ -6,82 +6,64 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 00:30:22 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/02 01:39:34 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/07 04:13:33 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-size_t	ft_strbitssplit(char *s, char c)
+size_t	ft_bits(const char *s, char c)
 {
 	size_t	is_bit;
 	size_t	bits;
+	size_t	i;
 
+	i = 0;
 	is_bit = 0;
 	bits = 0;
-	while (*s)
+	while (s[i])
 	{
-		if (*s == c && is_bit == 0)
-			s++;
-		if (*s == c && is_bit == 1)
-		{
-			s++;
+		if (s[i] == c && is_bit != 0)
 			is_bit = 0;
+		if (s[i] != c && is_bit == 0)
+		{
 			bits++;
-		}
-		else
-		{
 			is_bit = 1;
-			s++;
 		}
+		while (s[i] && s[i] == c && is_bit == 0)
+			i++;
+		while (s[i] && s[i] != c && is_bit != 0)
+			i++;
 	}
-	if (*(s - 1) != c)
-		bits++;
 	return (bits);
-}
-
-size_t	ft_strlensplit(char *s, char c)
-{
-	size_t len;
-
-	len = 0;
-	while (*s)
-	{
-		if (*s == c)
-			s++;
-		else
-		{
-			len++;
-			s++;
-		}
-	}
-	return (len);
 }
 
 char	**ft_strsplit(char const *s, char c)
 {
 	char	**split;
-	char	*s1;
-	size_t	countbit;
+	size_t	i;
+	size_t	start;
+	size_t	end;
 	size_t	bits;
 
-	s1 = (char *)s;
-	bits = ft_strbitssplit(s1, c);
-	countbit = 0;
-	split = (char **)malloc(sizeof(**split) * (ft_strlensplit(s1, c) + bits));
-	if (split)
+	split = NULL;
+	bits = 0;
+	i = 0;
+	if (s && (split = (char **)malloc(sizeof(*split) * (ft_bits(s, c) + 1))))
 	{
-		while (countbit < bits)
+		while (s[i])
 		{
-			while (*s1 == c)
-				s1++;
-			while (*s1 != c)
-				*split[countbit]++ = *s1++;
-			*(split[countbit]) = '\0';
-			countbit++;
+			while (s[i] && s[i] == c)
+				i++;
+			start = i;
+			while (s[i] && s[i] != c)
+				i++;
+			end = i;
+			if (end > start)
+				split[bits++] = ft_strsub(s, start, end - start);
 		}
-		return (split);
 	}
-	return (NULL);
+	split[bits] = NULL;
+	return (split);
 }
