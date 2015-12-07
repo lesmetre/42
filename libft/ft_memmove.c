d5fe32302e524884731d7164b82a93e0958a00e9
mpressen <mpressen@student.42.fr>
2015-12-07 06:51:25 +0100

    add tmp

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,23 +6,38 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/28 02:14:05 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/02 23:51:21 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/07 06:48:57 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	char		*dst1;
-	const char	*src1;
+	char	*tmp;
+	char	*dst1;
+	char	*src1;
+	size_t	i;
 
-	dst1 = dst;
-	src1 = src;
-	while (len > 0)
+	if (!(dst) || !(src))
+		return (NULL);
+	dst1 = (char *)dst;
+	src1 = (char *)src;
+	tmp = (char *)malloc(sizeof(*tmp) * len);
+	if (!(tmp))
+		return (NULL);
+	i = 0;
+	while (i < len)
 	{
-		dst1[len - 1] = src1[len - 1];
-		len--;
+		tmp[i] = src1[i];
+		i++;
 	}
+	i = 0;
+	while (i < len)
+	{
+		dst1[i] = tmp[i];
+		i++;
+		}
 	return (dst);
 }
