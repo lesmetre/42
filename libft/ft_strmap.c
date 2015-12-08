72cb39fe91f09c153053d945078ebb4a5efee3ce
mpressen <mpressen@student.42.fr>
2015-12-08 03:58:30 +0100

    Update ft_strmap.c

--- libft/ft_strmap.c
+++ libft/ft_strmap.c
@@ -19,19 +19,16 @@ char	*ft_strmap(char const *s, char (*f)(char))
 	size_t	i;
 
 	i = 0;
-	if (s && f)
+	str = (char *)malloc(sizeof(*str) * (ft_strlen(s) + 1));
+	if (str)
 	{
-		str = (char *)malloc(sizeof(*str) * (ft_strlen(s) + 1));
-		if (str)
+		while (s[i])
 		{
-			while (s[i])
-			{
-				str[i] = f(s[i]);
-				i++;
-			}
-			str[i] = '\0';
-			return (str);
+			str[i] = f(s[i]);
+			i++;
 		}
+		str[i] = '\0';
+		return (str);
 	}
 	return (NULL);
 }
