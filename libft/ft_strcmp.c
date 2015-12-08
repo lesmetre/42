addbf45fba586a1af9a61264f65daef900e4663b
mpressen <mpressen@student.42.fr>
2015-12-08 01:21:37 +0100

    suppression protection

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/01 01:38:39 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/07 00:22:50 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/08 01:20:49 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,6 @@ int		ft_strcmp(const char *s1, const char *s2)
 	int s2len;
 	int n;
 
-	if (s1 == NULL || s2 == NULL)
-		return (0);
 	s1len = ft_strlen(s1);
 	s2len = ft_strlen(s2);
 	if (s1len >= s2len)
