b2831c0676917d6169ec45c372db988d726b396c
Maximilien PRESSENSE <mpressen@e1r7p12.42.fr>
2015-12-10 09:34:08 +0100

    modif uncharted char et ajout f

---
+++ libft/ft_lstnew.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/12/10 08:44:49 by mpressen          #+#    #+#             */
+/*   Updated: 2015/12/10 09:28:24 by mpressen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memdup(const void *src, size_t n)
+{
+	void	*ret;
+
+	ret = NULL;
+	if (!(ret = (void *)malloc(sizeof(*src) * n)))
+		return (NULL);
+	ft_memcpy(ret, src, n);
+	return (ret);
+}
+
+t_list	*ft_lstnew(void	const *content, size_t content_size)
+{
+	t_list	*list;
+
+	list = NULL;
+	if (!(list = (t_list*)malloc(sizeof(*list))))
+		return (NULL);
+	if (content = NULL)
+	{
+		list->content = NULL;
+		list->content = 0;
+	}
+	else
+	{
+		list->content = ft_memdup(content, content_size);
+		list->content_size = content_size;
+	}
+	list->next = NULL;
+	return (list);
+}
--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/27 23:08:34 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/02 23:50:25 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/10 09:33:11 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,15 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	char		*str1;
-	const char	*str2;
+	unsigned char		*str1;
+	unsigned char		*str2;
 
-	str1 = dst;
-	str2 = src;
+	str1 = (unsigned char)dst;
+	str2 = (unsigned char)src;
 	while (n > 0)
 	{
 		str1[n - 1] = str2[n - 1];
 		n--;
 	}
-	return (dst);
+	return (str1);
 }
