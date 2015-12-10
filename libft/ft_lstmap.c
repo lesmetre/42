b0508058d08ae9f7a7c0e9327837328bb4933703
Maximilien PRESSENSE <mpressen@e1r7p14.42.fr>
2015-12-10 23:44:55 +0100

    ajout f

--- libft/ft_lstmap.c
+++ libft/ft_lstmap.c
@@ -6,20 +6,27 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 10:22:00 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/10 10:26:29 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/10 23:38:02 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstiter(t_list *lst, void (*f) (t_list *elem))
+void	ft_lstmap(t_list *lst, t_list *(*f) (t_list *elem))
 {
-	t_list	*list;
+	t_list	*new;
+	t_list	*begin;
 
+	if (!lst || !f)
+		return (NULL);
+	begin = NULL;
+	new = (*f)(lst);
+	begin = new;
 	while (lst)
 	{
-		list = lst->next;
-		f(lst);
-		lst = list;
+		new->next = (*f)(lst->next);
+		new = new->next;
+		lst = lst->next;
 	}
+	return (begin);
 }
