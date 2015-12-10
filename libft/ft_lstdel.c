39276fc4c557af35fc28c5bbfbe7c8c65933ad64
Maximilien PRESSENSE <mpressen@e1r7p14.42.fr>
2015-12-11 00:30:28 +0100

    ajout free tmp

--- libft/ft_lstdel.c
+++ libft/ft_lstdel.c
@@ -6,23 +6,23 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/10 09:54:53 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/11 00:02:26 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/11 00:29:13 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 void	ft_lstdel(t_list **alst, void (*del) (void*, size_t))
 {
-	t_list	*list;
 	t_list	*tmp;
 
-	list = *alst;
-	while (list)
+	while (*alst)
 	{
-		tmp = list->next;
-		del(list->content, list->content_size);
+		tmp = (*alst)->next;
+		del((*alst)->content, (*alst)->content_size);
 		ft_memdel((void **)alst);
-		list = tmp;
+		*alst = tmp;
 	}
+	free(tmp);
 }
