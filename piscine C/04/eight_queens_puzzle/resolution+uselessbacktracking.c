b946abb19b53cf6bd3d745a7cc9f3d4a95af4678
Maximilien PRESSENSE <mpressen@e1r3p6.42.fr>
2015-12-23 19:39:22 +0100

    resolu

--- piscine C/04/eight_queens_puzzle/resolution+uselessbacktracking.c
+++ piscine C/04/eight_queens_puzzle/resolution+uselessbacktracking.c
@@ -6,7 +6,7 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/22 14:55:28 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/23 19:33:32 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/23 19:30:51 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 int    resolution(char **tab, int queens)
 {
     int		column;
+//	int		queen_on_line;
 	int		solutions;
 
 	solutions = 0;
@@ -27,14 +28,36 @@ int    resolution(char **tab, int queens)
 		ft_putchar('\n');
 		return (solutions);
 	}
-    while (++column < 8)            //resolution bruteforce
+    while (++column < 8)            //resolution complexe
     {
         if (no_queen_check(queens, column, tab))
         {
            tab[queens][column] = '1';
+//			queen_on_line = 1;
             solutions += resolution(tab, queens + 1);
             tab[queens][column] = '.';
         }
     }
+/*	if (!(queen_on_line))
+	{
+		while (--queens)			//backtracking
+		{
+			column = 8;
+			while (column--)
+			{
+				if (tab[queens][column] == '1')
+				{
+					tab[queens][column] = '.';
+					while (++column < 8)
+						if (no_queen_check(queens, column, tab))
+						{
+							tab[queens][column] = '1';
+							solutions += resolution(tab, queens + 1);
+						}
+					column = 0;
+				}
+			}
+		}
+		}*/
 	return(solutions);
 }
