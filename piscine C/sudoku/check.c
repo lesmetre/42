a1e00ee0051bb7fcd6c6a64b66a154b76741933b
Maximilien PRESSENSE <mpressen@e1r3p6.42.fr>
2015-12-28 23:05:43 +0100

    without printf debug

--- piscine C/sudoku/check.c
+++ piscine C/sudoku/check.c
@@ -6,22 +6,17 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/23 20:01:37 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/28 22:55:47 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/28 23:01:07 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sudoku.h"
-# include <stdio.h>
 
 int		check_line(int line, int column, char c, char **tab)
 {
 	while (column--)
 		if (tab[line][column] == c)
-		{
-			printf("%c deja dans la ligne %d\n", c, line);
 			return (0);
-		}
-	printf("pas de %c dans la ligne %d\n", c, line);
 	return (1);
 }
 
@@ -29,11 +24,7 @@ int		check_column(int line, int column, char c, char **tab)
 {
 	while (line--)
 		if (tab[line][column] == c)
-		{
-			printf("%c deja dans la colonne\n", c);
 			return (0);
-		}
-	printf("pas de %c dans la colonne %d\n", c, column);
 	return (1);
 }
 
@@ -44,24 +35,18 @@ int		check_block(int line, int column, char c, char **tab)
 
 	i = 0;
 	line = line - (line % 3);
+	column = column - (column % 3);
 	while (i < 3)
 	{
-		printf("check de la ligne %d\n", line + i);
-		column = column - (column % 3);
 		j = 0;
 		while (j < 3)
 		{
-			printf("check de la colonne %d sur la ligne %d\n", column + j, line + i);
 			if (tab[line][column] == c)
-			{
-				printf("%c deja dans le block\n", c);
 				return (0);
-			}
 			j++;
 		}
 		i++;
 	}
-	printf("pas de %c dans le block\n", c);
 	return (1);
 }
 
@@ -69,16 +54,12 @@ int		check_emptycase(int line, int column, char **tab)
 {
 	if (tab[line][column] != '.')
 		return (0);
-	printf("la case de la ligne %d et de la colonne %d est vide\n", line, column);
 	return (1);
 }
 
 int		check(int line, int column, char c, char **tab)
 {
 	if (check_line(line, column, c, tab) && check_column(line, column, c, tab) && check_block(line, column, c, tab))
-	{
-		printf("on peut placer %c a la ligne %d et a la colonne %d\n", c, line, column);
 		return (1);
-	}
 	return (0);
 }
--- piscine C/sudoku/resolution.c
+++ piscine C/sudoku/resolution.c
@@ -6,12 +6,11 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/23 20:00:59 by mpressen          #+#    #+#             */
-/*   Updated: 2015/12/28 22:48:27 by mpressen         ###   ########.fr       */
+/*   Updated: 2015/12/28 23:01:31 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "sudoku.h"
-#include <stdio.h>
 
 int		resolution(char **tab)
 {
@@ -38,19 +37,16 @@ int		resolution(char **tab)
 		{
 			if (check(line, column, c, tab))
 			{
-				printf("case rempli par %c\n", c);
 				tab[line][column] = c;
 				if (resolution(tab))
 					return (1);
 				tab[line][column] = '.';
 			}
-			printf("test infructueux avec %c\n", c);
 		}
 		return (0);
 	}
 	else
 	{ 
-		printf("sudoku complet");
 		print_tab(tab);
 		return (1);
 	}
