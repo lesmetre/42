4f2a4cb0f56df6effc18a057dd26ac8920bbd99a
Maximilien PRESSENSE <mpressen@e1r9p14.42.fr>
2016-01-08 08:57:21 +0100

    bonus

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/31 00:21:08 by mpressen          #+#    #+#             */
-/*   Updated: 2016/01/08 04:06:17 by mpressen         ###   ########.fr       */
+/*   Updated: 2016/01/08 07:42:55 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE	10000000
+# define BUFF_SIZE	2
 
 int		get_next_line(int const fd, char **line);
 void	free_tab(char **tab);
