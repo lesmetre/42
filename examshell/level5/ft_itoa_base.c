0542af71d68123daa1947b22b7570d4aa1514190
Maximilien PRESSENSE <mpressen@e1r3p4.42.fr>
2016-02-02 02:45:25 +0100

    ajout itoabase

--- examshell/level5/ft_itoa_base.c
+++ examshell/level5/ft_itoa_base.c
@@ -1,26 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*   ft_itoa_base.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mpressen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/02 02:42:22 by mpressen          #+#    #+#             */
-/*   Updated: 2016/02/01 21:56:32 by mpressen         ###   ########.fr       */
+/*   Updated: 2016/02/02 02:12:08 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-
-static int		ft_strlen(char *s)
-{
-    int  i;
-
-    i = 0;
-    while (s[i])
-        i++;
-    return (i);
-}
 
 static char    *ft_strdup(char *s1)
 {
@@ -28,7 +18,7 @@ static char    *ft_strdup(char *s1)
     int     i;
 
     i = -1;
-    if ((dup = (char*)malloc(sizeof(ft_strlen(s1) + 1))))
+    if ((dup = (char*)malloc(sizeof(*dup) * 12)))
     {
         while (s1[++i])
             dup[i] = s1[i];
@@ -43,12 +33,6 @@ static int		ft_itoa_base_len(int value, int base)
 	int		len;
 
 	len = 1;
-	if (value < 0)
-	{
-		value *= -1;
-		if (base == 10)
-			len++;
-	}
 	while (value > base - 1)
 	{
 		value /= base;
@@ -61,7 +45,9 @@ char			*ft_itoa_base(int value, int base)
 {
 	char	*str;
 	int		len;
+	int		sign;
 
+	sign = 0;
 	if (base < 2 || base > 16)
 		return (NULL);
 	if (value == -2147483648)
@@ -71,7 +57,13 @@ char			*ft_itoa_base(int value, int base)
 		else
 			return (NULL);
 	}
-	len = ft_itoa_base_len(value, base);
+	if (value < 0)
+	{
+		value *= -1;
+		if (base == 10)
+			sign++;
+	}
+	len = ft_itoa_base_len(value, base) + sign;
 	if ((str = (char*)malloc(sizeof(*str * len + 1))))
 	{
 		str[len] = '\0';
@@ -83,7 +75,7 @@ char			*ft_itoa_base(int value, int base)
 				str[len] = value % base + 'A' - 10;
 			value /= base;
 		}
-		if (value < 0 && base == 10)
+		if (sign)
 			str[0] = '-';
 		return (str);
 	}
