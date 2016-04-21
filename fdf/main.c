0de85a610ce7d4ab2a7b6268c8f8fd5250990579
Maximilien PRESSENSE <mpressen@e3r13p2.42.fr>
2016-04-21 19:18:37 +0200

    norme main

--- fdf/main.c
+++ fdf/main.c
@@ -6,7 +6,7 @@
 /*   By: mpressen <mpressen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/12 01:10:10 by mpressen          #+#    #+#             */
-/*   Updated: 2016/04/21 19:07:26 by mpressen         ###   ########.fr       */
+/*   Updated: 2016/04/21 19:17:34 by mpressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 static void		display(t_fdfparam **addr_param, char *file, t_fdflist *list)
 {
 	t_fdfparam	*param;
-	
+
 	if (!(param = (t_fdfparam*)malloc(sizeof(*param))))
 	{
 		ft_error_malloc("display");
 		exit(1);
 	}
-	if (ft_strrchr(file, '/'))
-		param->file = ft_strrchr(file, '/') + 1;
-	else
-		param->file = file;
+	param->file = (ft_strrchr(file, '/')) ? ft_strrchr(file, '/') + 1 : file;
 	param->width = 2560;
 	param->height = 1400;
 	param->mlx = mlx_init();
